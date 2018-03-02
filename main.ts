b14a912179b73e36b3c29fef5dcbb7552450d774
Marco Antônio <ma.souza.junior@gmail.com>
2018-03-02 16:22:12 +0100

    FEA : Load devdocs page inside electron window

--- main.ts
+++ main.ts
@@ -1,14 +1,31 @@
-import * as electron from 'electron'
+import { app, BrowserWindow, BrowserView } from 'electron'
+import { resolve } from 'path'
 
-const { app, BrowserWindow } = electron
-let mainWindow
+let mainWindow: BrowserWindow
 
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 220,
-    height: 100,
-    title: "Base electron TypeScript"
+    width: 800,
+    height: 600,
+    title: "DevDocs - Desktop",
+    icon: resolve(__dirname, 'images/icon.png')
   })
+
+  const view = new BrowserView({
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  view.webContents.loadURL('https://devdocs.io')
+  mainWindow.setBrowserView(view)
+
+  const resizeView = () => {
+    const { width, height } = mainWindow.getBounds()
+    view.setBounds({ x: 0, y: 0, width, height })
+  }
+  // Resize BrowserView when BrowserWindow is resized
+  mainWindow.on('resize', resizeView)
+  resizeView()
 }
 
 app.on('ready', createMainWindow)
